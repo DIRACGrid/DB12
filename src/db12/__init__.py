62f9e0557e8bc23edd22b080d9426e440d9006b9
aldbr <aldbr@outlook.com>
2021-10-25 11:02:48 +0200

    Add a norm correction for python3

--- src/db12/__init__.py
+++ src/db12/__init__.py
@@ -3,8 +3,7 @@ from __future__ import absolute_import
 __all__ = [
     "single_dirac_benchmark",
     "multiple_dirac_benchmark",
-    "jobslot_dirac_benchmark",
-    "wholenode_dirac_benchmark"
+    "wholenode_dirac_benchmark",
 ]
 
 from .benchmark import *
